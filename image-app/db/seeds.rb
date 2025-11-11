e58dabea9272c7952f804d024775dc5bb0b71e39
mikeya <moonsubaru@gmail.com>
2025-11-11 10:59:25 +0900

    サインインページ

--- image-app/db/seeds.rb
+++ image-app/db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.where(id: [1,2,3]).destroy_all
+User.create(id: 1, email_address: "dev@example.com", password: "password1")
+User.create(id: 2, email_address: "dev2@example.com", password: "password2")
+User.create(id: 3, email_address: "dev3@example.com", password: "password3")
